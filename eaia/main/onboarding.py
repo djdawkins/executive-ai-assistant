53c02f672342c0f1a6ab9e09fea430cc87606432
djdawkins <ddawkns@gmail.com>
2025-06-08 00:00:14 -0400

    fix: update triage response logic for onboarding and initial offer statuses

--- eaia/main/onboarding.py
+++ eaia/main/onboarding.py
@@ -5,14 +5,12 @@ from langchain_openai import ChatOpenAI
 from langgraph.store.base import BaseStore
 
 from eaia.schemas import (
-    State,
-    ContactConfirmResponse,
-    ResponseTextDraft,
-    Question,
-    Ignore,
-    text_template,
+    State
+    , text_template
+    , land_survey_text
 )
 from eaia.main.config import get_config
+from datetime import datetime
 
 TEXT_WRITING_INSTRUCTIONS = """You are {full_name}'s executive assistant. You are a top-notch executive assistant who cares about {name} performing as well as possible.
 
@@ -20,18 +18,17 @@ TEXT_WRITING_INSTRUCTIONS = """You are {full_name}'s executive assistant. You ar
 
 {name} gets lots of texts. This has been determined to be a text from a prospect that is in the on boarding phase.
 
-Your job is to help {name} respond. You can do this in a few ways.
+Your job is to help {name} decide how to respond. 
+You can do this by responding with one of the following keywords:
 
-# Using the `ContactConfirm` tool
+# ContactConfirmResponse 
 
 First, the contact confirm information is a boolean that indicates whether the contact is confirmed. 
 The contact confirm information value is {contact_confirm}.
 Follow this exactly,
-If the contact confirm information is "False", you should always respond with the `ContactConfirmResponse` tool.
+If the contact confirm information is "False", you should always respond with the `ContactConfirmResponse`.
 
-ALWAYS draft texts as if they are coming from {name}. Never draft them as "{name}'s assistant" or someone else.
 
-Do NOT make up texts.
 """
 draft_prompt = """{instructions}
 
@@ -41,72 +38,56 @@ Remember to call a tool correctly! Use the specified names exactly - not add `fu
 
 async def onboarding(state: State, config: RunnableConfig, store: BaseStore):
     """Write a text to a customer."""
+    # print("config:", config)
     model = config["configurable"].get("model", "gpt-4o")
     llm = ChatOpenAI(
         model=model,
-        temperature=0,
-        parallel_tool_calls=False,
-        tool_choice="required",
+        temperature=0
     )
-    tools = [
-        ContactConfirmResponse,
-        ResponseTextDraft,
-        Question,
-    ]
+
     messages = state.get("messages") or []
-    if len(messages) > 0:
-        tools.append(Ignore)
+    prospect = state.get("prospect") or []
+
     prompt_config = get_config(config)
     namespace = (config["configurable"].get("assistant_id", "default"),)
-    # key = "text_thread"
-    # result = await store.aget(namespace, key)
-    # if result and "data" in result.value:
-    #     text_thread = result.value["data"]
-    # else:
-    #     await store.aput(namespace, key, {"data": prompt_config["text_thread"]})
-    #     text_thread = prompt_config["text_thread"]
-    # key = "random_preferences"
-    # result = await store.aget(namespace, key)
-    # if result and "data" in result.value:
-    #     random_preferences = result.value["data"]
-    # else:
-    #     await store.aput(
-    #         namespace, key, {"data": prompt_config["background_preferences"]}
-    #     )
-    #     random_preferences = prompt_config["background_preferences"]
-    # key = "response_preferences"
-    # result = await store.aget(namespace, key)
-    # if result and "data" in result.value:
-    #     response_preferences = result.value["data"]
-    # else:
-    #     await store.aput(namespace, key, {"data": prompt_config["response_preferences"]})
-    #     response_preferences = prompt_config["response_preferences"]
+
     _prompt = TEXT_WRITING_INSTRUCTIONS.format(
         contact_confirm=state["prospect"]["contact_info_confirmed"],
         name=prompt_config["name"],
         full_name=prompt_config["full_name"],
         background=prompt_config["background"],
     )
-    print(state["messages"][-1])
     input_message = draft_prompt.format(
         instructions=_prompt,
-        # text=text_template.format(
-        #     first_name=state["prospect"]["first_name"],
-        #     last_name=state["prospect"]["last_name"],
-        #     prop_street=state["prospect"]["prop_street"],
-        #     prop_city=state["prospect"]["prop_city"],
-        #     prop_state=state["prospect"]["prop_state"],
-        # ),
     )
 
-    model = llm.bind_tools(tools)
+    # model = llm.bind_tools(tools)
     messages = [{"role": "user", "content": input_message}] + messages
-    i = 0
-    while i < 5:
-        response = await model.ainvoke(messages)
-        if len(response.tool_calls) != 1:
-            i += 1
-            messages += [{"role": "user", "content": "Please call a valid tool call."}]
-        else:
-            break
-    return {"draft": response, "messages": [response]}
+    response = await llm.ainvoke(messages)
+    print("response:", response.content)
+    
+    if response.content not in ["ContactConfirmResponse", "LandSurvey", "Question", "Ignore"]:
+        messages += [{"role": "user", "content": "Please choose a key value."}]
+
+    elif response.content == "ContactConfirmResponse":
+
+        text = text_template.format(
+            prop_street=state["prospect"]["prop_street"] ,
+            prop_city=state["prospect"]["prop_city"],
+            prop_state=state["prospect"]["prop_state"],
+            first_name=state["prospect"]["first_name"],
+            last_name=state["prospect"]["last_name"],
+        )
+        messages += [{"role": "user", "content": text}]
+        prospect["contact_info_confirmed"] = True
+        prospect["status"] = "onboarding"
+        prospect["follow_up_date"] = datetime.now().date()
+
+    elif response.content == "LandSurvey":
+        text = land_survey_text
+        messages += [{"role": "user", "content": text}]
+        prospect["contact_info_confirmed"] = True
+        prospect["status"] = "ready_for_initial_offer"
+        prospect["follow_up_date"] = datetime.now().date()
+
+    return {"prospect": prospect, "messages": messages}
--- eaia/main/triage.py
+++ eaia/main/triage.py
@@ -25,8 +25,9 @@ Texts that are worth responding to:
 
 The lead status for the prospect is "{lead_status}".
 Follow this exactly the the lead status for the prospect determines how you triage the text. 
-If the lead status is "new", you should always respond `onboard`.
+If the lead status is "new" or "onboarding", you should always respond `onboard`.
 
+If the lead status is "ready_for_initial_offer", you should always respond `text`.
 There are also other things that {name} should know about, but don't require a text response. For these, you should notify {name} (using the `notify` response). Examples of this include:
 {triage_notify}
 
