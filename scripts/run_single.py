67951174cce0ef44e6ec58c99064ac27f6eafc5c
djdawkins <ddawkns@gmail.com>
2025-06-07 23:58:37 -0400

    fix: correct message type and update thread_id in run_single.py

--- scripts/run_single.py
+++ scripts/run_single.py
@@ -11,11 +11,13 @@ from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 async def main():
     client = get_client(url="http://127.0.0.1:2024")
     # initial state
-    intial_messages = [HumanMessage(content="""Hi, my name is DJ from Rosedale Capital. Would you be interested in a proposal for your vacant lot in Cape Coral?reply "Yes" for more info. reply "End" to quit.""")]
+    intial_messages = [AIMessage(content="""Hi, my name is DJ from Rosedale Capital. Would you be interested in a proposal for your vacant lot in Cape Coral?reply "Yes" for more info. reply "End" to quit.""")]
+                    #    , HumanMessage(content="""Yes""")
+                    #    AIMessage(content="""Hi DJ, thanks for reaching out! I'm interested in hearing more about the proposal for my vacant lot in Cape Coral. Please provide me with the details.""")]
     
     text: TextData = {
         "id": "test",
-        "thread_id": "test",
+        "thread_id": "test_5",
         "from_phone_number": "+14802909934",
         "text_content": "yes",
         "send_time": "2024-12-26T13:13:41-08:00",
