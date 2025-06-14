762b249767438fafd583f0182d22adfdb4063ecd
djdawkins <ddawkns@gmail.com>
2025-06-14 18:43:03 -0400

    fix: correct message type and update thread_id and contact confirmation status in run_single script

--- scripts/run_single.py
+++ scripts/run_single.py
@@ -12,13 +12,13 @@ async def main():
     client = get_client(url="http://127.0.0.1:2024")
     # initial state
     intial_messages = [AIMessage(content="""Hi, my name is DJ from Rosedale Capital. Would you be interested in a proposal for your vacant lot in Cape Coral?reply "Yes" for more info. reply "End" to quit.""")
-                       , HumanMessage(content="""Okay great. 
+                       , AIMessage(content="""Okay great. 
 The property address is 2531 E Monroe St Phoenix, AZ and your name is Sheet Metal Workers Assoc Local Union 359 correct?""")
                     #    AIMessage(content="""Hi DJ, thanks for reaching out! I'm interested in hearing more about the proposal for my vacant lot in Cape Coral. Please provide me with the details.""")]
                 ]
     text: TextData = {
         "id": "test",
-        "thread_id": "test_6",
+        "thread_id": "test_9",
         "from_phone_number": "+14802909934",
         "text_content": "yes",
         "send_time": "2024-12-26T13:13:41-08:00",
@@ -31,7 +31,7 @@ The property address is 2531 E Monroe St Phoenix, AZ and your name is Sheet Meta
         "first_name":"Sheet",
         "last_name":"Metal Workers Assoc Local Union 359",
         "opt_in":True,
-        "contact_info_confirmed":True,
+        "contact_info_confirmed":False,
         "messages_sent":0,
         "status":"onboarding",
         "updated_at":"2025-06-01T21:13:06.63518",
