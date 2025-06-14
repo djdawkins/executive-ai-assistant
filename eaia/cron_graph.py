f5bda7cfd65d53781e7080c69fffec2910b8105e
djdawkins <ddawkns@gmail.com>
2025-06-14 18:43:37 -0400

    fix: enhance lead processing logic to include thread_id and follow-up date in runs

--- eaia/cron_graph.py
+++ eaia/cron_graph.py
@@ -6,6 +6,16 @@ import uuid
 import hashlib
 from langgraph.graph import StateGraph, START, END
 from eaia.main.config import get_config
+from eaia.schemas import EmailData, TextData, ProspectData
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from supabase import create_client, Client
+import os
+from datetime import date
+# Initialize Supabase client
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+
+sb_client: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 client = get_client()
 
@@ -15,37 +25,101 @@ class JobKickoff(TypedDict):
 
 
 async def main(state: JobKickoff, config):
+    #TODO: Fix add thread id and follow up date column to the leads table
+    today = date.today().isoformat()
+    prospects, _ = await sb_client.table('leads') \
+                .select('phone_number', '"prop_street"', '"prop_city"', \
+                        '"prop_state"', '"prop_zip"', '"first_name"', '"last_name"', \
+                            'status', 'opt_in', 'contact_info_confirmed', 'last_message_sent', \
+                                'last_message_received', 'thread_id') \
+                .eq('follow_up_date', today) \
+                .limit(100) \
+                .execute()
+        
     minutes_since: int = state["minutes_since"]
     email = get_config(config)["email"]
 
-    # TODO: This really should be async
-    for email in fetch_group_emails(email, minutes_since=minutes_since):
-        thread_id = str(
-            uuid.UUID(hex=hashlib.md5(email["thread_id"].encode("UTF-8")).hexdigest())
-        )
-        try:
-            thread_info = await client.threads.get(thread_id)
-        except httpx.HTTPStatusError as e:
-            if "user_respond" in email:
-                continue
-            if e.response.status_code == 404:
-                thread_info = await client.threads.create(thread_id=thread_id)
-            else:
-                raise e
-        if "user_respond" in email:
-            await client.threads.update_state(thread_id, None, as_node="__end__")
-            continue
-        recent_email = thread_info["metadata"].get("email_id")
-        if recent_email == email["id"]:
-            break
-        await client.threads.update(thread_id, metadata={"email_id": email["id"]})
+    for prospect in await prospects:
 
-        await client.runs.create(
-            thread_id,
-            "main",
-            input={"email": email},
-            multitask_strategy="rollback",
-        )
+        if prospect["status"] == "ready_for_initial_offer":
+            # TODO: Add messages here to triggrer the notify node if needed
+            intial_messages = []
+            # TODO: Add dummy text data here if needed
+            text: TextData = {
+                # "id": "test",
+                # "thread_id": prospect["thread_id"] or str(uuid.uuid4()),
+                # # "thread_id": prospect["thread_id"] or str(uuid.uuid4()),
+
+                # "from_phone_number": "+14802909934",
+                # "text_content": "yes",
+                # "send_time": "2024-12-26T13:13:41-08:00",
+                # "to_phone_number": "+1234567890",
+            }
+
+            prospect: ProspectData = {
+                "phone_number":prospect["phone_number"],
+                "first_name":prospect["first_name"],
+                "last_name":prospect["last_name"],
+                "opt_in":prospect["opt_in"],
+                "contact_info_confirmed":prospect["contact_info_confirmed"],
+                "status":prospect["status"],
+                "prop_street": prospect["prop_street"],
+                "prop_city":prospect["prop_city"],
+                "prop_state":prospect["prop_state"],
+                "prop_zip":prospect["prop_zip"],
+                # is the below needed?
+                "message_history":[prospect["last_message_sent"], prospect["last_message_received"]],
+            }
+
+            thread_id = str(
+                uuid.UUID(hex=hashlib.md5(text["thread_id"].encode("UTF-8")).hexdigest())
+            )
+
+            await client.runs.create(
+                thread_id,
+                "main",
+                input={"text": text, "messages": intial_messages, "prospect": prospect},
+                multitask_strategy="rollback",
+            )        
+        else:
+            # TODO: Add messages here if needed, depends on how we populate the database
+            intial_messages = []
+            # TODO: Add text data here if needed, depends on how we populate the database
+            text: TextData = {
+                # "id": "test",
+                # "thread_id": prospect["thread_id"] or str(uuid.uuid4()),
+                # # "thread_id": prospect["thread_id"] or str(uuid.uuid4()),
+
+                # "from_phone_number": "+14802909934",
+                # "text_content": "yes",
+                # "send_time": "2024-12-26T13:13:41-08:00",
+                # "to_phone_number": "+1234567890",
+            }
+
+            prospect: ProspectData = {
+                "phone_number":prospect["phone_number"],
+                "first_name":prospect["first_name"],
+                "last_name":prospect["last_name"],
+                "opt_in":prospect["opt_in"],
+                "contact_info_confirmed":prospect["contact_info_confirmed"],
+                "status":prospect["status"],
+                "prop_street": prospect["prop_street"],
+                "prop_city":prospect["prop_city"],
+                "prop_state":prospect["prop_state"],
+                "prop_zip":prospect["prop_zip"],
+                "message_history":[prospect["last_message_sent"], prospect["last_message_received"]],
+            }
+
+            thread_id = str(
+                uuid.UUID(hex=hashlib.md5(text["thread_id"].encode("UTF-8")).hexdigest())
+            )
+
+            await client.runs.create(
+                thread_id,
+                "main",
+                input={"text": text, "messages": intial_messages, "prospect": prospect},
+                multitask_strategy="rollback",
+            )
 
 
 graph = StateGraph(JobKickoff)
